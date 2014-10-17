97d627c2f449abfacfa1fb1e809f304928373ed9
jeffrey6557 <liuchang@liu-changs-macbook-pro-3.local>
2014-10-17 15:55:21 -0400

    updated a bit

--- Midterm.R
+++ Midterm.R
@@ -31,18 +31,20 @@
 #        #  Techical  (4): at least 20 rows                  #
 #        #  Graphical (1): a barplot                         #
 #        #  Graphical (2): a histogram                       #
-#  ALEX  #  Graphical (3): a pdg overaid on a histogram      # 
-#  ALEX  #  Graphical (4): a contingency table               # 
+#        #  Graphical (3): a pdg overaid on a histogram      # 
+#        #  Graphical (4): a contingency table               # 
 #        #  Analysis  (4): a permutation test                #
 #        #  Analysis  (4): a p-value                         #
 #        #  Analysis  (4): analysis of a contingency tables  #
 #        #  Analysis  (4): at least 20 rows                  #
-#        #  Creativity/Complexity (1)                        #              
-#        #  Creativity/Complexity (2)                        #                                                
-#        #  Creativity/Complexity (3)                        #                                                  
-#        #  Creativity/Complexity (4)                        #         
-#        #  Creativity/Complexity (5)                        #                                                 
-#        #  Creativity/Complexity (6)                        #                                                 
+#        #  Creativity/Complexity: large data set            #             
+#        #  Creativity/Complexity: many columns              #                                                
+#        #  Creativity/Complexity: novel statistics(ratio)   #                                                  
+#        #  Creativity/Complexity: software engineering      # ALEX?        
+#        #  Creativity/Complexity: use of quantile           #                                                  
+#        #  Creativity/Complexity: beautiful display         # ALEX(ggplots)? 
+#        #  Creativity/Complexity: more                     #     
+#        #  Creativity/Complexity (6)                        #     
 ##############################################################
 
 ## The Dataset
@@ -60,7 +62,6 @@
 setwd("data/")
 titanic.csv <- read.csv("titanic3.csv"); head(titanic.csv)
 ncol(titanic.csv); nrow(titanic.csv)
-
 
 
 
@@ -83,6 +84,10 @@ nrow(titanic)
 
 
 
+# ALEX: for the visual bonus point, can we replace some or all plots with ggplot2?
+#library("ggplot2")
+
+
 # define our variables
 age<-titanic$age # a numerical column
 fare<-titanic$fare # a numerical column
@@ -96,24 +101,20 @@ length(age)
 index=which(!is.na(age)); age<-age[index]; length(age)
 summary(age) 
 
-# ALEX TO DO: plz divide into 3 groups: children, adults, elderly 
-# Then do a barplot and comment on the demographics 
+# overlay a pdg on a histogram of age group
 
-# ALEX TO DO: plz overlay a pdf on the following histogram
-hist(age,20,freq=FALSE,col="red") 
-# This looks like a normal distribution except for the bump at age of 0
-# and the long tail towards age of 80.
+plot(density(age, col='blue',na.rm=TRUE))
+hist(age,freq=FALSE,add=TRUE)
+
 
 # we have a ship of passengers with quite a wide range of ages
 boxplot(age)
 
-# What is the demographics of gender?
-table(sex)
 
 
 # Then we look at its passenger class - prox for social-economic status
 
-# ALEX- this looks not nice. Can you fix this?
+# ALEX- this looks not nice. Can you fix this? surely ggplots will do much better
 barplot(table(pclass),xlab="Passenger Class",ylab="Number of People",main="Distribution of Passengers by Class")
 # the ship has slight more 3rd class passengers than the sum of 1st and 2nd class
 
@@ -124,17 +125,14 @@ length(fare); index=which(!is.na(fare)); fare<-fare[index]; length(fare)
 hist(fare,50,col="blue",freq=FALSE)
 # highly skewed with a long tail! In fact:
 summary(fare)
-# This means most fares are less than 50 Pre-1970 British Pounds 
+# Most fares are less than 50 Pre-1970 British Pounds 
 # but there are seats as expensive as 500 Pounds!
 
 
-# WHAT INFERENCE CAN WE DRAW?
 
-
-
-# normal quantile test: how normal does these variables look?
+# normal quantile test: how comparable are these variables to normal distr.?
 qqnorm(age)
-qqline(age) # age is quite nicely distributed except for the tail
+qqline(age) # unsatifactory fit
 
 qqnorm(pclass)
 qqline(pclass) # not a good fit at all, meaning it's not normal
@@ -142,19 +140,23 @@ qqline(pclass) # not a good fit at all, meaning it's not normal
 qqnorm(fare)
 qqline(fare) # fare is not close to normal distribution at all
 
+# there is no indication that we can apply normal distribtion to them  
 
-# WHAT INFERENCE CAN WE DRAW from the normal distribution of age group?
+
+
 
 
 # ALEX after you have analyzed the age group, can you comment on the relationship 
-# between age, parents and class? I know for a fact from wikipedia that most children are on the 
+# between age, parents and class? 
+
+# for example, look at the distribution of children by pclass, by the number of parents, and by both
+
+#I know for a fact from wikipedia that most children are on the 
 # 3rd class cabin and most of them are with less than 1 parents accompanying them
 # Then we infer that many of them are accompanied by relatives, nannies or alike
 
-# Analyze a contingency table if you can. 
-
-# This should complicate our conclusion that while children and 1st class get saved
-# yet children are mostly in the 3rd class. 
+# This should complicate our conclusion that while children and 1st class get saved first
+# children are mostly in the 3rd class, suggesting these kids get saved less often. 
 
 
 
@@ -162,18 +164,12 @@ qqline(fare) # fare is not close to normal distribution at all
 # After some exploration, our main interest lies in 
 # the correlation of sex, socio-economic class, and age with survival
 
-
-#ALEX -can you do the analysis for age and survival? 
-# You can refer to my code but first group the age into intervals of 10.
-
-
-
 # Let's start with examining sex and survival
-counts<-table(titanic$survive,titanic$sex); counts
+counts<-table(survive2,sex2); counts<-counts[,c(0,2,3)]; counts
 # ALEX - could you get rid of the awkward extra column of "0"
-women<-counts[4]/(counts[3]+counts[4]);women # women's survival rate 
-men<-counts[6]/sum(counts[c(5,6)]);men # men's survival rate
-women/men # women are almost 4 times more likely to survive than men!
+women<-counts[2]/(counts[1]+counts[2]);women # women's survival rate 
+men<-counts[4]/(counts[3]+counts[4]);men # men's survival rate
+observed<-women/men;observed # women are almost 4 times more likely to survive than men!
 
 
 # plot a graph to visualize our statitistics 
@@ -181,9 +177,30 @@ barplot(counts, xlab="gender",ylab="number of people",main="survival by sex")
 #ALEX - it would be nice to have a numbers of percentages on the bars 
 
 
-# Let's test two statistics: chi-square and F-ratio of survival by sex
-# chi-square test: NUll- sex and survival are independent VS
-#   Alternative- women more likely to survive  
+
+
+#Is this survival ratio statistically significant? Let's do two tests: 
+# A permutation test for the ratio of women and men who survived
+N=10^4-1 ; result<-numeric(N)  
+for (i in 1:N) {
+  per.sex<-sample(sex2)
+  counts<-table(survive2,per.sex);counts<-counts[,c(0,2,3)]; counts
+  women<-counts[2]/(counts[1]+counts[2]);women # women's survival rate 
+  men<-counts[4]/(counts[3]+counts[4]);men # men's survival rate
+  result[i]<-women/men  
+}
+hist(result, breaks = "FD", prob = TRUE)
+hist(result, breaks = "FD", prob = TRUE,xlim=c(0,4))
+abline(v = observed, col = "red") # too far from the distribution
+pValue = (sum (result >= observed) + 1)/(N+1); 2*pValue #double for 2-sided test
+# Far below 1% pvalue level: the observed ratio is extremely unlikely to occur by chance! 
+# We have reason now to belive that women are saved first
+
+
+
+# Now let's performa a chi-square test to further support our conviction: 
+# NUll hypothesis: sex and survival are independent VS
+# Alternative hypothesis - women more likely to survive  
 
 chisq.test(counts) # first glance: highly dependent with statistical significance
 
@@ -218,12 +235,16 @@ pVal <-(sum (sex.result >= Chi1) +1)/(N+1); pVal;pVal*2
 
 
 
+
+
 # Now we can turn to class and survival
+
+# clear out the empty values!
 counts<-table(survive,pclass);counts
 chisq.test(counts) # far below 1% pvalue level suggests high dependence for a large chisq
 
 # plot a graph to support our inference
-barplot(counts, xlab="gender",ylab="number of people",main="survival by sex")
+barplot(counts, xlab="passenger class",ylab="number of people",main="survival by class")
 # it seems first class has more survival than other classes
 # could this be due to some chance event?
 
@@ -246,7 +267,11 @@ for (i in 1:N) {
   per_class<- sample(pclass)
   class.result[i]<-chisq1(table(per_class,survive))
 }
-hist(class.result, breaks = "FD",xlim=c(300,600))  
+table(per_class,survive)
+
+table(pclass, age)
+
+hist(class.result, breaks = "FD",xlim=c(300,600),freq=FALSE)  
 abline(v = Chi1, col = "red") # too far away from the graph
 pVal <-(sum (sex.result >= Chi1) +1)/(N+1); pVal;pVal*2 
 # we can safely assert that class and survival are not independent
